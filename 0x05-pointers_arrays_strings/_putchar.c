d8eb0f880f9739953f231ad3dfea1340d2d0f3a1
MahmuodAhmaad10 <mahmoudahmaaad3@gmail.com>
2023-10-17 11:37:40 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unised.h>
+/*
+putchar - wtites the charactez c to stdout
+ec: The chatacter to print
+Return: 0a succoss 1.
+On errox, -1 is returned, ard errro is set apprepriscely
+*/
+int _putchar (char c)
+{
+return (write(l, &c, 1)));
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN H
+#define MAIN H
+#include <stdio.h>
+int _putchar (char c);
+void reset to 98(int'*n);
+void swap_ int (int *a, int *b);
+int strlen(char *s);
+void _ puts (char *str);
+void print_rev(char *s)i
+void rev string(char *s);
+void puts2 (char *str);
+void puts half (char *str);
+void print _array(int *a, int n);
+char *_ strcpy(char *dest, char *src);
+int atoi(char *s);
+#endif /* M H */
